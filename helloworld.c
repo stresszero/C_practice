d160034a2c41d7ab1479fa8c9e600868f030bb24
stresszero <restouri@naver.com>
2022-11-20 18:24:15 +0900

    wip

--- helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> // preprocessor directive
-
-int main(void) {
-    int age = 20;
-    printf("%d\n", age);
-    age = 21;
-    printf("%d\n", age);
-    return 0;
-}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // preprocessor directive
+
+int main(void) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
