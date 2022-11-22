60b82e863a0e1e084f24302bf420907b515c4386
stresszero <restouri@naver.com>
2022-11-22 12:46:58 +0900

    sip

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h> // preprocessor directive
 
+void function_without_return();
+
 int main(void) {
-    int n = 2;
-    switch (n) {
-        case 1:
-            printf("사과 \n");
-        case 2:
-            printf("딸기\n");
-        case 3:
-            printf("수박\n");
-    }
+    function_without_return();
     return 0;
+}
+
+void function_without_return() {
+    printf("No return function. \n");
 }
