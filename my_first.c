19d0b8f6b19bfbccdafe4f58990908a424fd0c72
stresszero <restouri@naver.com>
2022-12-03 16:24:42 +0900

    sip

--- my_first.c
+++ my_first.c
@@ -1,7 +1,12 @@
-#include <stdio.h> // standard input/output
+#include <stdio.h>
 
 int main()
 {
-	printf("1st");
+	int n1, n2, n3, n4;
+
+	n1 = 2;
+	n2 = n1 * n1;
+	n3 = n2 * n1;
+	
 	return 0;
 }
