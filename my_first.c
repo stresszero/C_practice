19dfbf2ec8d0339e1798ec775c3a8218607c4214
stresszero <restouri@naver.com>
2022-12-04 11:46:38 +0900

    sip

--- my_first.c
+++ my_first.c
@@ -1,11 +1,11 @@
-// #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 
 int main()
 {
-	int i = 0;
-	scanf("%d", &i); // & : ampersand
-	printf("Values is %d\n", i);
+	int i = 0, j = 0;
+	printf("Input two integers\n");
+	scanf("%d%d", &i, &j);
+	int sum = i + j;
+	printf("%d plus %d = %d\n", i, j, sum);
 	return 0;
 }
